210643e9456d44d120facc2c84a485b7fa779f26
DevBuddy <devbuddy@local>
2025-04-16 19:16:54 -0400

    fix bugs in add and multiply methods

--- main.py
+++ main.py
@@ -2,13 +2,13 @@
 
 class Calculator:
     def add(self, a, b):
-        return a + b + 1
+        return a + b
     
     def subtract(self, a, b):
         return a - b
     
     def multiply(self, a, b):
-        return (a - b)*50
+        return a * b
     
     def divide(self, a, b):
         if b == 0:
