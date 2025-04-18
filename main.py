1b5d1e8694a425fb579ad5ed5c1b243404acc992
Philliaezer <reddragondggda@gmail.com>
2025-04-18 15:01:07 -0300

    refactor: Change if statement for simple case conditions
    This commit is intended to improve the "pythonic level" of the code.

--- main.py
+++ main.py
@@ -23,50 +23,51 @@ class Calculator:
             )
 
 def main():
-    calc = Calculator()
+	calc = Calculator()
     
-    print(Colors.YELLOW)
-    print("Welcome to the Basic Calculator!")
-    print(Colors.END)
-    print("--------------------------------")
-    # ↑ you could use print("-"*32)
+	print(Colors.YELLOW)
+	print("Welcome to the Basic Calculator!")
+	print(Colors.END)
+	print("--------------------------------")
+	# ↑ you could use print("-"*32)
+	
+	print(Colors.NEGATIVE)
+	print("\nOperations:")
+	print(Colors.END)
+	print("1. Addition")
+	print("2. Subtraction")
+	print("3. Multiplication")
+	print("4. Division")
     
-    print(Colors.NEGATIVE)
-    print("\nOperations:")
-    print(Colors.END)
-    print("1. Addition")
-    print("2. Subtraction")
-    print("3. Multiplication")
-    print("4. Division")
-    
-    try:
-        choice = input("\nEnter your choice (1-4): ")
-        num1 = float(input("Enter first number: "))
-        num2 = float(input("Enter second number: "))
-        
-        if choice == '1':
-            result = calc.add(num1, num2)
-            print("\n")
-            print(f"{num1} + {num2} = {result}")
-        elif choice == '2':
-            result = calc.subtract(num1, num2)
-            print("\n")
-            print(f"{num1} - {num2} = {result}")
-        elif choice == '3':
-            result = calc.multiply(num1, num2)
-            print("\n")
-            print(f"{num1} * {num2} = {result}")
-        elif choice == '4':
-            result = calc.divide(num1, num2)
-            print(f"{num1} / {num2} = {result}")
-        else:
-            print("Invalid choice.")
-    except ValueError:
-        print("Invalid input. Please enter numbers only.")
-    except Exception as e:
-        print(f"An error occurred: {e}")
-    print("\n")
-    print("Goodbye!")
+	try:
+		choice = input("\nEnter your choice (1-4): ")
+		num1 = float(input("Enter first number: "))
+		num2 = float(input("Enter second number: "))
+		
+		match choice:
+			case '1':
+				result = calc.add(num1, num2)
+				print("\n")
+				print(f"{num1} + {num2} = {result}")
+			case '2':
+				result = calc.subtract(num1, num2)
+				print("\n")
+				print(f"{num1} - {num2} = {result}")
+			case '3':
+				result = calc.multiply(num1, num2)
+				print("\n")
+				print(f"{num1} * {num2} = {result}")
+			case '4':
+				result = calc.divide(num1, num2)
+				print(f"{num1} / {num2} = {result}")
+			case _:
+				print("Invalid choice.")
+	except ValueError:
+		print("Invalid input. Please enter numbers only.")
+	except Exception as e:
+		print(f"An error occurred: {e}")
+		print("\n")
+		print("Goodbye!")
 
 if __name__ == "__main__":
     main()
